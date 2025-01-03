168957da214bed6e464d5a94f184f69f6fee6a44
sandeep-kahom <sandeep@kahom.tech>
2025-01-03 13:03:11 +0530

    Add files via upload
    V1

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFormsProj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            lblText.Text = "Welcome" + tBox.Text + ". Your age is " + textBox1.Text;
+        }
+
+        private void tBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblText.Text = "";
+        }
+    }
+}
